7b4911e42d56395a98c4115e007b005edcca9a70
Philip Gonzalez <pgonzalez115@gmail.com>
2018-10-19 10:01:55 -0400

    Add basic node8 Dockerfile to both services

--- hashing_svc/index.js
+++ hashing_svc/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 
-const PORT = process.env.port || 3001;
+const PORT = process.env.port || 3002;
 
 const app = express();
 app.use(bodyParser.json());
